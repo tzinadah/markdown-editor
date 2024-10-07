0920d74f915f71334fb70d2866aa8576a01268bc
Talal Abuziandah <primezinadah@gmail.com>
2024-10-07 18:48:17 +0100

    Add markdown functionality

--- src/MarkdownPreview.js
+++ src/MarkdownPreview.js
@@ -1,11 +1,14 @@
 import {MarkdownContext} from "./App"
 import { useContext } from "react";
+import { marked } from "marked";
 
 function MarkdownPreview(){
+    
     const [text] = useContext(MarkdownContext);
-    return <div id="preview">
-        {text}
+
+    return <div id="preview" dangerouslySetInnerHTML={{__html: marked.parse(text)}}>
     </div>
+
 }
 
 export default MarkdownPreview
